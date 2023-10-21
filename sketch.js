a863855f0f7e48d3ee0e490ecae12810df045734
Ben Shattock <ben.shattock@icloud.com>
2023-10-21 12:32:06 +0100

    Changed Predict function and Removed Pyodide

--- sketch.js
+++ sketch.js
@@ -378,16 +378,16 @@ function getOppositeMove(move) {
 }
 
 function transformFields() {
-    let polynomialFields = [...fields];
-    polynomialFields.push(fields[4] * fields[14] * fields[20]);
-    polynomialFields.push(fields[2] * fields[6] * fields[22]);
-    polynomialFields.push(fields[3] * fields[18] * fields[23]);
-    polynomialFields.push(fields[15] * fields[16] * fields[21]);
-    polynomialFields.push(fields[0] * fields[7] * fields[10]);
-    polynomialFields.push(fields[1] * fields[11] * fields[19]);
-    polynomialFields.push(fields[9] * fields[13] * fields[17]);
-    polynomialFields.push(fields[5] * fields[8] * fields[12]);
-    return polynomialFields;
+    let newFields = 
+    new onnx.Tensor(new Float32Array([[[
+        [0.0, 0.0, fields[12], fields[13], 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, fields[14], fields[15], 0.0, 0.0, 0.0, 0.0],
+        [fields[5], fields[4], fields[20], fields[21], fields[16], fields[17], fields[9], fields[8]],
+        [fields[7], fields[6], fields[22], fields[23], fields[18], fields[18], fields[11], fields[10]],
+        [0.0, 0.0, fields[2], fields[3], 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, fields[0], fields[1], 0.0, 0.0, 0.0, 0.0],
+    ]]]), 'float32', [1, 1, 8, 6])
+    return newFields
 }
 
 function chooseMove(move, pMoves) {
@@ -411,14 +411,23 @@ function chooseMove(move, pMoves) {
     return newMove;
 }
 
+async function predict(input) {
+    const sess = new onnx.InferenceSession();
+    await sess.loadModel('./data/onnx-model.onnx');
+    const outputMap = await sess.run([input])
+    const outputTensor = outputMap.values().next().value
+    console.log(outputTensor.data)
+    
+    return outputTensor.data
+}
+
 function nextMove() {
     let move;
     let key = fields.toString();
     if (moves[key] === undefined || moves[key].length == 0) {
         moves[key] = [...possibleMoves];
-        let polynomialFields = transformFields();
-        predict = pyscript.runtime.globals.get('predict');
-        move = predict(polynomialFields);
+        let newFields = transformFields();
+        move = predict(newFields);
         if (move == getOppositeMove(lastMove)) {
             moves[key].splice(moves[key].indexOf(move), 1);
             move = moves[key][Math.floor(Math.random() * moves[key].length)];
